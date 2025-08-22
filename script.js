d54f6877faec1a8bac82aa2b976fe09777de58cb
Pedro Henrique <pedro.castro109@aluno.ce.gov.br>
2025-08-22 17:56:32 +0000

    alterado script e index

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ let nome = (prompt("Digite seu nome"))
 let idade = parseInt(prompt("Digite sua idade")) 
 let senha = parseInt(prompt("Digite sua senha"))
 
- elseif(idade < 13); {
+if(idade < 13) {
     alert("Desculpe não foi possivel se cadastrar! (idade minima 13) ")
- } elseif(idade > 13); {
+ } else {
     alert("Cadastro realizado com sucesso!") 
  } 
 // LOGIN
 let verificarNome = prompt("Digite novamente seu nome:")
 let verifcarSenha = parseInt(prompt("Digite novamente sua senha:"))
 
-if (verificarNome == nome && verifcarSenha == senha ); {
+if (verificarNome == nome && verifcarSenha == senha ) {
     alert(`Login relizado com sucesso! Bem-vido, ${nome} `)
-} if (verificarNome === nome && verifcarSenha === senha); {
+} if (verificarNome === nome && verifcarSenha === senha) {
     alert(`Nome ou senha incorretos. Tente novamente. `)
 }
 
