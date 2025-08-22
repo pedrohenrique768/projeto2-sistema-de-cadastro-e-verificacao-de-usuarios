cb2882d8b2d0cd1b9d9fcbc5b2a9a29b8155547e
Pedro Henrique <pedro.castro109@aluno.ce.gov.br>
2025-08-22 17:51:19 +0000

    criado documento script e  index

---
+++ script.js
@@ -0,0 +1,20 @@
+// CADASTRO
+let nome = (prompt("Digite seu nome"))
+let idade = parseInt(prompt("Digite sua idade")) 
+let senha = parseInt(prompt("Digite sua senha"))
+
+ elseif(idade < 13); {
+    alert("Desculpe não foi possivel se cadastrar! (idade minima 13) ")
+ } elseif(idade > 13); {
+    alert("Cadastro realizado com sucesso!") 
+ } 
+// LOGIN
+let verificarNome = prompt("Digite novamente seu nome:")
+let verifcarSenha = parseInt(prompt("Digite novamente sua senha:"))
+
+if (verificarNome == nome && verifcarSenha == senha ); {
+    alert(`Login relizado com sucesso! Bem-vido, ${nome} `)
+} if (verificarNome === nome && verifcarSenha === senha); {
+    alert(`Nome ou senha incorretos. Tente novamente. `)
+}
+
